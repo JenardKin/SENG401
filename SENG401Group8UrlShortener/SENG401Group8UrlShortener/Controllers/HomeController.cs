39c8b4339afabdd3c62d2924308c0c0b4b9dc5f0
jenardkin.cabia <jenardkin.cabia@cs.ucalgary.ca>
2018-02-09 17:57:04 -0700

    "windows LinkShortener"

--- SENG401Group8UrlShortener/SENG401Group8UrlShortener/Controllers/HomeController.cs
+++ SENG401Group8UrlShortener/SENG401Group8UrlShortener/Controllers/HomeController.cs
@@ -1,30 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+using SENG401Group8UrlShortener.Models;
 
 namespace SENG401Group8UrlShortener.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        public ActionResult LinkShortener(string longURL)
         {
+            string shortURL = longURL;
+            ViewData["Title"] = "Link Shortener";
+            //If string from post was null, then this field isn't displayed
+            if (longURL != null)
+            {
+                //Randomly choose and int... This will actually be the PK value in the DB
+                Random rnd = new Random();
+                int value = rnd.Next(0, 100);
+                //Algorithm for shortended link
+                string encoding = Shortener.GetShortEncoding(value);
+                shortURL = "Encoding of PK: " + value.ToString() + " equals: " + encoding;
+            }
+            ViewData["shortURL"] = shortURL;
+
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Error()
         {
-            ViewBag.Message = "Your application description page.";
-
+            //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ SENG401Group8UrlShortener/SENG401Group8UrlShortener/Models/LinkDatabase.cs
@@ -0,0 +1,132 @@
+﻿using MySql.Data.MySqlClient;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace LinkShortener.Models.Database
+{
+    public partial class LinkDatabase : AbstractDatabase
+    {
+        private LinkDatabase() { }
+
+        public static LinkDatabase getInstance()
+        {
+            if(instance == null)
+            {
+                instance = new LinkDatabase();
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// Gets a long URL based on the id of the short url
+        /// </summary>
+        /// <param name="id">The id of the short url</param>
+        /// <throws type="ArgumentException">Throws an argument exception if the short url id does not refer to anything in the database</throws>
+        /// <returns>The long url the given short url refers to</returns>
+        public string getLongUrl(string id)
+        {
+            string query = @"SELECT * FROM " + dbname + ".shortenedLinks "
+                + "WHERE id=" + id + ";";
+
+            if(openConnection() == true)
+            {
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if(reader.Read() == true)
+                {
+                    return reader.GetString("original");
+                }
+                else
+                {
+                    //Throw an exception indicating no result was found
+                    throw new ArgumentException("No url in the database matches that id.");
+                }
+            }
+            else
+            {
+                throw new Exception("Could not connect to database.");
+            }
+        }
+
+        /// <summary>
+        /// Saves the longURL to the database to be accessed later via the id that is returned.
+        /// </summary>
+        /// <param name="longURL">The longURL to be saved</param>
+        /// <returns>The id of the url</returns>
+        public string saveLongURL(string longURL)
+        {
+            string query = @"INSERT INTO " + dbname + ".shortenedLinks(original) "
+                + @"VALUES('" + longURL + @"');";
+
+            if(openConnection() == true)
+            {
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.ExecuteNonQuery();
+
+                command.CommandText = "SELECT * FROM " + dbname + ".shortenedLinks WHERE id = LAST_INSERT_ID();";
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if(reader.Read() == true)
+                {
+                    string result = reader.GetInt64("id").ToString();
+                    closeConnection();
+                    return result.ToString();
+                }
+                else
+                {
+                    closeConnection();
+                    throw new Exception("Error: LAST_INSERT_ID() did not work as intended.");
+                }
+            }
+            else
+            {
+                throw new Exception("Could not connect to database");
+            }
+        }
+    }
+
+    public partial class LinkDatabase : AbstractDatabase
+    {
+        private static LinkDatabase instance = null;
+
+        private const String dbname = "Links";
+        public override String databaseName { get; } = dbname;
+
+        protected override Table[] tables { get; } =
+        {
+            // This represents the database schema
+            new Table
+            (
+                dbname,
+                "shortenedLinks",
+                new Column[]
+                {
+                    new Column
+                    (
+                        "id", "INT(64)",
+                        new string[]
+                        {
+                            "NOT NULL",
+                            "UNIQUE",
+                            "AUTO_INCREMENT"
+                        }, true
+                    ),
+                    new Column
+                    (
+                        "original", "VARCHAR(300)",
+                        new string[]
+                        {
+                            "NOT NULL"
+                        }, false
+                    )
+                }
+            )
+        };
+    }
+}
