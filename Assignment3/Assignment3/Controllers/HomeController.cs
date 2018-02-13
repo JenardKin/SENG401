2fce9caa363a814959b033c3b371e48f0c32e558
natejacko <nathan.jackson@ucalgary.ca>
2018-02-13 14:16:29 -0700

    Minor changes and comments

--- Assignment3/Assignment3/Controllers/HomeController.cs
+++ Assignment3/Assignment3/Controllers/HomeController.cs
@@ -7,48 +7,79 @@ namespace Assignment3.Controllers
 {
     public class HomeController : Controller
     {
+        //Defualt action of home controller is linkshortener rather than index (changed in route config)
+        //Action takes an optional parameter (longURL) - The url to be shortened
         public ActionResult LinkShortener(string longURL)
         {   
-            var dbInstance = LinkDatabase.getInstance();
-            dbInstance.createDB();
-
+            //Save the short url as the long url (currently)
             string shortURL = longURL;
+            //Add the current title to the view bag (for display in the tab on a web broswer)
             ViewBag.Title = "Link Shortener";
-            //If string from post was null, then this field isn't displayed
+            //If string from post was null, then this if statement is never entered
             if (longURL != null)
             {
-                string shortUrl_iD = dbInstance.saveLongURL(longURL);
-                string encoding = Shortener.GetShortEncoding(Int32.Parse(shortUrl_iD));
+                //Get the DB instance (and if necessary the DB itself)
+                var dbInstance = LinkDatabase.getInstance();
+                dbInstance.createDB();
+                //Save the longurl in the db and save the return as primary key
+                string pk = dbInstance.saveLongURL(longURL);
+                //Encode the PK
+                string encoding = Shortener.GetShortEncoding(Int32.Parse(pk));
+                //Get the host and port parts of our website url
                 string urlHost = HttpContext.Request.Url.Host;
                 string urlPort = HttpContext.Request.Url.Port.ToString();
+                //If no port, ignore adding it to the shortened url
                 if (urlPort != "")
                 {
+                    //Short url looks like: <website name>:<port>/Home/Go/<encoded pk>
                     shortURL = urlHost + ":" + urlPort + "/Home/Go/" + encoding;
                 }
                 else
                 {
+                    //Short url looks like: <website name>/Home/Go/<encoded pk>
                     shortURL = urlHost + "/Home/Go/" + encoding;
                 }
             }
+            //Return the shortened (or null) short url to the view
             ViewBag.shortURL = shortURL;
-
+            //Render the view
             return View();
         }
+        //Action to redirect to 
         public void Go(string id = null)
         {
+            //Get the db instance
             var dbInstance = LinkDatabase.getInstance();
 
             if (id != null)
             {
                 int decoding = Shortener.GetLongDecoding(id);
-                string longUrl = dbInstance.getLongUrl(decoding.ToString());
-                //Ideally, with the above ID we will squery the DB and get the respective url and redirect to that page
-                Response.Redirect(longUrl);
+                try
+                {
+                    string longUrl = dbInstance.getLongUrl(decoding.ToString());
+                    //Ideally, with the above ID we will query the DB and get the respective url and redirect to that page
+                    Response.Redirect(longUrl);
+                }
+                catch (ArgumentException)
+                {
+                    //If long url couldn't be found, redirect back to our homepage
+                    string urlHost = HttpContext.Request.Url.Host;
+                    string urlPort = HttpContext.Request.Url.Port.ToString();
+                    if (urlPort != "")
+                        Response.Redirect(urlHost + ":" + urlPort);
+                    else
+                        Response.Redirect(urlHost);
+                }
             }
             else
             {
                 //Otherwise, redirect back to our homepage
-                Response.Redirect(HttpContext.Request.Url.Host);
+                string urlHost = HttpContext.Request.Url.Host;
+                string urlPort = HttpContext.Request.Url.Port.ToString();
+                if (urlPort != "")
+                    Response.Redirect(urlHost + ":" + urlPort);
+                else
+                    Response.Redirect(urlHost);
             }
         }
     }
--- Assignment3/Assignment3/Models/Shortener.cs
+++ Assignment3/Assignment3/Models/Shortener.cs
@@ -1,32 +1,53 @@
-﻿using System;
-namespace LinkShortener.Models
+﻿namespace LinkShortener.Models
 {
     public static class Shortener
     {
+        // Reference for this encoding/decoding link-shortener algorithm is from:
+        //https://stackoverflow.com/questions/742013/how-to-code-a-url-shortener
+
+        //The characters used to encode the url
         public static readonly string characters = "ABCEDFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        //The length of characters (36)
         public static readonly int charCount = characters.Length;
 
+        //Method to encode an integer to a string
+        //Takes an int (correlating to the PK of the DB table)
+            //Hence the int MUST be a POSITIVE integer
         public static string GetShortEncoding(int i)
         {
-            if (i == 0)
-                return characters[0].ToString();
-
+            //Empty initialize the return
             string s = "";
 
+            //Loop while i > 0 (characters left to be added to the encoding string)
             while (i > 0)
             {
-                s = characters[i % charCount] + s;
+                //Check if the modulo of i and length is 0, if so use the last character in our encoding characters
+                //Otherwise, take modulo of i and length then subtract 1
+                //This differs from the algorithm we referenced as we wanted to ensure the first entry to the DB
+                    //i.e. PK == 1
+                //Leads to an encoding of the string "A"
+                if (i % charCount == 0)
+                    s = characters[charCount - 1] + s;
+                else
+                    s = characters[i % charCount - 1] + s;
+                //Divide i by length of characters (C# does floor division)
                 i = i / charCount;
             }
+            //Return the encoding
             return s;
         }
+        //Method to decode a string and return its respective PK in our DB
         public static int GetLongDecoding(string s)
         {
+            //Begin i at 0
             int i = 0;
+            //Loop through all characters in the string (left-right)
             foreach (char c in s)
             {
-                i = (i * charCount) + characters.IndexOf(c);
+                //This converts our base 32 string to an int (starting at 1) iteratively
+                i = (i * charCount) + characters.IndexOf(c) + 1;
             }
+            //Return the decoded int
             return i;
         }
     }
