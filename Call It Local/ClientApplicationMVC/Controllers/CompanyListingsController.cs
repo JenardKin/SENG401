08e3856f6c65519185efdba66dcf81572033b8ef
natejacko <njacko@shaw.ca>
2018-03-27 21:17:47 -0600

    Things look a little nicer now. Also some error checking performed

--- Call It Local/ClientApplicationMVC/Controllers/CompanyListingsController.cs
+++ Call It Local/ClientApplicationMVC/Controllers/CompanyListingsController.cs
@@ -97,44 +97,72 @@ namespace ClientApplicationMVC.Controllers
 
         public ActionResult DisplayReviews(string id)
         {
-            if(Globals.isLoggedIn() == false)
+            //Check if user is logged in
+            if (Globals.isLoggedIn() == false)
             {
                 return RedirectToAction("Index", "Authentication");
             }
+            //Check the the company name was not null
             if ("".Equals(id))
             {
                 return View("Index");
             }
+            ViewBag.CompanyName = id;
+            ServiceBusConnection connection = ConnectionManager.getConnectionObject(Globals.getUser());
+            if (connection == null)
+            {
+                //If can't connect to db, return false to view
+                ViewBag.CompanyExists = false;
+                return View("DisplayReviews");
+            }
+            GetCompanyInfoRequest infoRequest = new GetCompanyInfoRequest(new CompanyInstance(id));
+            GetCompanyInfoResponse infoResponse = connection.getCompanyInfo(infoRequest);
+            if (!infoResponse.result)
+            {
+                //If company doesn't exist in db, return false to view
+                ViewBag.CompanyExists = false;
+                return View("DisplayReviews");
+            }
+            //Otherwise company eixsts
+            ViewBag.CompanyExists = true;
+            //This is the rest call url with url parameter
             var url = "http://localhost:49834/Home/GetCompanyReview/" + id;
+            //Open connection and submit the get request
             var companyReviewClient = new HttpClient();
             var content = companyReviewClient.GetStringAsync(url).Result;
+            //Get results
             ResponseReview result = JsonConvert.DeserializeObject<ResponseReview>(content);
             int? totalStars = 0;
             int totalReviews = 0;
-            foreach(var review in result.reviews)
+            string avgStars = "0.00";
+            //Check if any reviews were returned
+            if (result.reviews != null)
             {
-                totalStars += review.stars;
-                totalReviews++;
+                //Loop reviews calculating avg stars
+                foreach (var review in result.reviews)
+                {
+                    totalStars += review.stars;
+                    totalReviews++;
+                }
+                avgStars = ((float)totalStars / totalReviews).ToString("0.00");
             }
-            //Format to 2 decimal places
-            var avgStars = ((float)totalStars / totalReviews).ToString("0.00");
+            //Pass all to display
             ViewBag.AvgStars = avgStars;
-            ViewBag.CompanyName = id;
             ViewBag.ReviewList = result.reviews;
             return View("DisplayReviews");
         }
 
         public ActionResult SubmitReview(string review, string companyName, string stars)
         {
-            //SAVE REVIEW WITH POST
-            //http://localhost:49834/
-            //THIS IP NEEDS TO BE OF THE DEPLOYMENT
+            //Check if user is logged in
             if (Globals.isLoggedIn() == false)
             {
                 return RedirectToAction("Index", "Authentication");
             }
+            //Check that all info was provided
             if (review != "" && companyName != "" && stars != "")
             {
+                //Get username and timestamp then generate a review object
                 string username = Globals.getUser();
                 long timestamp = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
                 Review reviewObject = new Review()
@@ -149,11 +177,14 @@ namespace ClientApplicationMVC.Controllers
                 {
                     review = reviewObject
                 };
+                //Create client and submit post
                 var companyReviewClient = new HttpClient();
                 var jsonObject = JsonConvert.SerializeObject(reviewsObject);
                 var stringContent = new StringContent(jsonObject, Encoding.UTF8, "application/json");
+                //This url is the one where the rest call it to
                 var res = companyReviewClient.PostAsync("http://localhost:49834/Home/SaveCompanyReview", stringContent).Result.Content.ReadAsStringAsync().Result;
                 Response resObject = JsonConvert.DeserializeObject<Response>(res);
+                //If response bad, redirect indicating bad request, otherwise, good
                 if(resObject.response == "failure")
                 {
                     return RedirectToAction("DisplayCompany", new { id = companyName, responseStatus = "Error occured when submitting review" });
